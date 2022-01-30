363d4bb81af858de9947ff6d0928ecc67cd0c788
Lucky Bandil <96731082+Luckybandil@users.noreply.github.com>
2022-01-30 22:49:04 +0530

    Commit Changed

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Linecomparision
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            LineComp lineComp = new LineComp();
+            lineComp.lc();
+
+        }
+    }
+}
